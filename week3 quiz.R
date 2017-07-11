1c980ea1b358ba2fe562a67b5b0b26815d29bc33
sagashin <sagashin@gmail.com>
2017-07-12 00:33:57 +0900

    Update week3 quiz.R

--- week3 quiz.R
+++ week3 quiz.R
@@ -16,9 +16,22 @@ library(jpeg)
 jp <- readJPEG("getdata%2Fjeff.jpg", native = TRUE)
 quantile(jp,probs = c(0.3,0.8))
 
-df2 <- read.csv("getdata%2Fdata%2FGDP.csv")
-df3 <- read.csv("getdata%2Fdata%2FEDSTATS_Country.csv")
+df2 <- read.csv("getdata-data-GDP.csv",skip = 4,nrows = 215,)
+df3 <- read.csv("getdata-data-EDSTATS_Country.csv")
+df4 <- merge(df2, df3, by.x = "X",by.y ="CountryCode")
+df4 <- arrange(df4, desc(X.1))
+sum(!is.na(df4$X.1))
+df4[13,]
 
-str(df3)
-summary(df2)
-names(df3)
+df5 <- filter(df4,df4$Income.Group =="High income: OECD" | df4$Income.Group == "High income: nonOECD") 
+df5 %>% group_by(Income.Group) %>% 
+        summarize(mean(X.1,na.rm = TRUE)) %>% 
+        print
+
+mean(df5$X.1,na.rm = TRUE)
+breaks <- quantile(df4$X.1,probs =c(0,0.2,0.4,0.6,0.8,1) ,na.rm = TRUE)
+df4$qtr <- cut(df4$X.1,breaks = breaks)
+table(df4$Income.Group,df4$qtr)
+
+
+
